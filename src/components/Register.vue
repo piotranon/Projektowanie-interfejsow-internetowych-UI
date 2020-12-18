e961595690760d86c4b67d8fdfc81afa05a1d267
piotranon <piotranon.1@gmail.com>
2020-12-18 19:11:15 +0100

    login, register route connections

---
+++ src/components/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="register"><h1>Register</h1></div>
+</template>
+<script>
+export default {
+  name: "Register"
+};
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
+import PageNotFound from "../components/PageNotFound.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "Home",
     component: Home
   },
@@ -15,6 +18,20 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: PageNotFound
   }
 ];
 
@@ -22,5 +39,23 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  if (isAuthenticated()) {
+    console.log("Zalogowany");
+    next();
+  }
+  console.log("Nie Zalogowany");
+  if (to.fullPath !== "/login" && to.fullPath !== "/register") {
+    next("/login");
+  } else next();
+});
+
+function isAuthenticated() {
+  return (
+    localStorage.permissions === "admin" ||
+    localStorage.permissions === "driver"
+  );
+}
 
 export default router;
